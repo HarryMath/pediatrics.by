92214879a846d99727a7b866ef1db37105d8d0bf
Mikita Bortnik <nikitabort22092000@gmail.com>
2023-04-02 17:33:18 +0300

    sdk initialized

--- src/app/sdk/schedule.sdk.ts
+++ src/app/sdk/schedule.sdk.ts
@@ -1,10 +1,51 @@
 import { DoctorDto } from 'src/app/sdk/dto/Doctor';
+import {ClientCreateDto, ClientDto} from "./dto/Client";
+import {EventCreateDto} from "./dto/Event";
 
 export const endpoint = 'http://localhost:80/api/'
 
 export class ScheduleSdk {
-  static async getDoctors(): Promise<DoctorDto[]> {
-    const response = await fetch(endpoint + 'doctors');
+
+  static doctors = {
+    get(search?: string): Promise<DoctorDto[]> {
+      return ScheduleSdk.get<DoctorDto[]>('doctors', { search });
+    },
+
+    getRoles(): Promise<string[]> {
+      return ScheduleSdk.get<string[]>('doctors/roles');
+    }
+  }
+
+  static clients = {
+    save(c: ClientCreateDto): Promise<ClientDto> {
+      return ScheduleSdk.post<ClientDto, ClientCreateDto>('clients', c);
+    }
+  }
+
+  static events = {
+    create(e: EventCreateDto): Promise<void> {
+      return ScheduleSdk.post<void, EventCreateDto>('events', e);
+    }
+  }
+
+  static async get<Res = void>(path: string, query?: Record<string, any>): Promise<Res> {
+    const queryString = Object
+      .entries(query || {})
+      .filter(([key, value]) => value !== undefined && value !== null && value?.length !== 0)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&');
+
+    let finalUrl = endpoint + path;
+    if (queryString) {
+      finalUrl += '?' + queryString;
+    }
+    const response = await fetch(endpoint, { method: 'GET' });
+    return await response.json();
+  }
+
+  static async post<Res = void, Req extends {} = {}>(path: string, payload?: Req): Promise<Res> {
+    const body = JSON.stringify(payload || {});
+    const response = await fetch(endpoint + path, { method: 'POST', body });
     return await response.json();
   }
 }
